fc99761cc5b3daef2f300f69916c99b50b3e80a6
GordonChen19 <chengordon8@gmail.com>
2023-10-03 16:08:31 +0800

    Simplified code

--- Storing.c
+++ Storing.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <stdbool.h>
+#include <string.h>
 typedef struct{
     uint16_t year; // 12 bits for year (limited to 0-4095)
     uint8_t month; // 4 bits for month (limited to 1-12)
@@ -16,17 +17,9 @@ typedef struct{
     uint8_t HOME_TEAM_WINS; 
 }Record; //20 Bytes (+1 Byte for data alignment)
 
-int compareFGPCT(const void *a, const void *b);
-int compareFGPCT(const void *a, const void *b){
-    const Record* recordA = (const Record*) a;
-    const Record* recordB = (const Record*) b;
-    // Compare based on FG_PCT_home field
-    if (recordA->FG_PCT_home < recordB->FG_PCT_home) return -1;
-    if (recordA->FG_PCT_home > recordB->FG_PCT_home) return 1;
-    return 0;
-}
+Record** DiskAllocation();
 
-int main() {
+Record** DiskAllocation() {
     FILE *file = fopen("games.txt","r");
 
     //Count the number of lines 
@@ -37,92 +30,75 @@ int main() {
         if(c=='\n') record_count++;
 
     }while(c!=EOF);
+
+    //////////////////////////////////////////////////////////////////
+
+    //Solution to Part 1
     
-    Record* Database=malloc(sizeof(Record)*record_count);
+    const int blockSize = 400;
+    const int recordSize = sizeof(Record);
+    const int recordsPerBlock = blockSize/recordSize;
+    const int blockQuantity = ceil(record_count/(blockSize/recordsPerBlock));
+    
 
-    file = fopen("games.txt","r");
-    //Copying file to database
-    if (file != NULL) {
-        char* record=malloc(sizeof(char)*110);
-        int index=-1;
-        while(fgets(record,110,file)){
-            if(index!=-1){
+    printf("Total number of records: %d\n",record_count); //record_count = 26651 Records
+    printf("Size of a record: %d\n",recordSize);
+    printf("Number of records stored in a block: %d\n",recordsPerBlock);
+    printf("Number of blocks for storing the data: %d\n",blockQuantity);
+
+    //////////////////////////////////////////////////////////////////
+
+    //Simulate accesses with a block as a unit
+
+    Record** Disk=malloc(sizeof(Record*)*blockQuantity);
+
+    file=fopen("games.txt","r");
+    
+
+    char* record=malloc(sizeof(char)*110);
+    fgets(record,110,file); //Ignore the first line of code
+
+    for(int block=0;block<blockQuantity;block++){
+        Disk[block]=malloc(sizeof(Record)*recordsPerBlock); 
+        for(int rec=0;rec<recordsPerBlock;rec++){ //Storing records in each block 
+            if(fgets(record,110,file)){
                 printf("%s",record);
-                // Database[index]=(Record*)malloc(sizeof(Record));
-                
                 float FT_PCT,FG_PCT,FG3_PCT;
-                
-
                 sscanf(record,"%2hhu/%2hhu/%4hu %u %hu %f %f %f %hhu %hhu %hhu",
-                    &Database[index].day,
-                    &Database[index].month,
-                    &Database[index].year,
-                    &Database[index].TEAM_ID_home,
-                    &Database[index].PTS_home,
+                    &Disk[block][rec].day,
+                    &Disk[block][rec].month,
+                    &Disk[block][rec].year,
+                    &Disk[block][rec].TEAM_ID_home,
+                    &Disk[block][rec].PTS_home,
                     &FG_PCT,
                     &FT_PCT,
                     &FG3_PCT,
-                    &Database[index].AST_home,
-                    &Database[index].REB_home,
-                    &Database[index].HOME_TEAM_WINS);
-
+                    &Disk[block][rec].AST_home,
+                    &Disk[block][rec].REB_home,
+                    &Disk[block][rec].HOME_TEAM_WINS);
                 FG_PCT*=1000;
                 FT_PCT*=1000;
                 FG3_PCT*=1000;
-                Database[index].FG_PCT_home=(int)FG_PCT;
-                Database[index].FT_PCT_home=(int)FT_PCT;
-                Database[index].FG3_PCT_home=(int)FG3_PCT;
-               
+                Disk[block][rec].FG_PCT_home=(int)FG_PCT;
+                Disk[block][rec].FT_PCT_home=(int)FT_PCT;
+                Disk[block][rec].FG3_PCT_home=(int)FG3_PCT;
+
             }
-            index++;
         }
-        fclose(file);
-
-        printf("%d",index);
-        printf("Printing first record");
-        printf("FG_PCT: %hu\n",Database[0].FG_PCT_home);
-        qsort(Database,record_count,sizeof(Record),compareFGPCT);
-        printf("Printing first sorted record");
-        printf("FG_PCT: %hu\n",Database[0].FG_PCT_home);
-
-        //Sorted records in block 
-
-        //Simulate accesses with a block as a unit
-
-
-
-        const int blockSize = 400;
-        const int recordSize = sizeof(Record);
-        const int recordsPerBlock = blockSize/recordSize;
-        const int blockQuantity = ceil(record_count/(blockSize/recordsPerBlock));
-        
-
-        printf("Total number of records: %d\n",record_count); //record_count = 26651 Records
-        printf("Size of a record: %d\n",recordSize);
-         printf("Number of records stored in a block: %d\n",recordsPerBlock);
-        printf("Number of blocks for storing the data: %d\n",blockQuantity);
-        
-
-        Record** Disk=malloc(sizeof(Record*)*blockQuantity);
-
-        int row=0;
-        for(int block=0;block<blockQuantity;block++){
-            Disk[block]=malloc(sizeof(Record)*recordsPerBlock);
-            int block_volume = 0;
-            int location_on_block = 0;
-            while(block_volume<=400)
-            {
-                Disk[block][0] = Database[row++];
-                location_on_block++;
-                block_volume+=recordSize;
-            }
-            // Insert record into the block
-        }
-
-        
-
     }
-    return 0;
+    fclose(file);
+
+    printf("Printing first record");
+    printf("FG_PCT: %hu\n",Disk[0][0].FG_PCT_home);
+
+    //////////////////////////////////////////////////////////////////
+    
+    return Disk;
 }
 
 
+int main(){ 
+    Record** Disk=DiskAllocation(); //Store records into blocks inside disk
+    
+
+}
